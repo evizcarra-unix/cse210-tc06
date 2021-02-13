09f4f10c5c3ea7ce7687fd1ca75f7f564a5202e8
Ezra Vizcarra <vizcarraezra@yahoo.com>
2021-02-13 14:36:17 -0700

    Files created and named respectively by classes, Console is done.

---
+++ mastermind/game/director.py
@@ -0,0 +1,11 @@
+from game.secret_code import Secret_Code
+from game.console import Console
+from game.move import Move
+from game.player import Player
+from game.roster import Roster
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    """
+    #TODO
